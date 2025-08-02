c28fcaf57f49650cd078aff1433850965ce9e775
Edward Dwemoh A <turfstudios.gh@gmail.com>
2025-08-02 09:55:53 +0000

    Add GitHub User Search project

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>GitHub User Search</h1>
+      <p>Start building your app...</p>
+    </div>
+  );
+}
+
+export default App;
