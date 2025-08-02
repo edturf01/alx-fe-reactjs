c9d9eaf24fae334959ecc0609d73027be09afa24
Edward Dwemoh A <turfstudios.gh@gmail.com>
2025-08-02 13:37:31 +0000

    Config

--- github-user-search/tailwind.config.js
+++ github-user-search/tailwind.config.js
@@ -1,9 +1,9 @@
+const typography = require('@tailwindcss/typography');
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [typography],
+};
