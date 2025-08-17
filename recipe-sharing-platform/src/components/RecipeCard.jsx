ea2e7246f90ec7a146696901190bfc49f39ba986
Edward Dwemoh A <turfstudios.gh@gmail.com>
2025-08-17 11:41:46 +0000

    feat: add Recipe Detail Page with React Router and Tailwind styling

---
+++ recipe-sharing-platform/src/components/RecipeCard.jsx
@@ -0,0 +1,19 @@
+
+import { Link } from "react-router-dom";
+
+function RecipeCard({ recipe }) {
+  return (
+    <div className="border rounded-lg p-4 shadow hover:shadow-lg transition">
+      <img src={recipe.image} alt={recipe.name} className="w-full h-48 object-cover rounded" />
+      <h3 className="text-xl font-semibold mt-2">{recipe.name}</h3>
+      <Link
+        to={`/recipe/${recipe.id}`}
+        className="text-blue-500 mt-2 inline-block"
+      >
+        View Recipe
+      </Link>
+    </div>
+  );
+}
+
+export default RecipeCard;
