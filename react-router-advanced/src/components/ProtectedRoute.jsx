79e6faa76ed3d89b1c89d4276844850dbfc4d4df
Edward Dwemoh A <turfstudios.gh@gmail.com>
2025-08-19 12:51:15 +0000

    Update ProtectedRoute to use useAuth hook

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 function ProtectedRoute({ children }) {
-  const isAuthenticated = false; // change to true to simulate login
+  const { isAuthenticated } = useAuth();
 
   if (!isAuthenticated) {
     return <Navigate to="/" replace />;
