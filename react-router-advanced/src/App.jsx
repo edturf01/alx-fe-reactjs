bd3dd142d901ad853fea33c14af39fc77af44437
Edward Dwemoh A <turfstudios.gh@gmail.com>
2025-08-19 12:48:03 +0000

    Create ProtectedRoute component and wrap Profile route

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,12 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile";
 import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/profile/*" element={<Profile />} />
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
